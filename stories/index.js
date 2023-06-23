bc4411517e7dee618fec23e1f6165dd8bc297b6e
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-06-23 11:48:13 -0700

    Add stories for Form component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 // DayListItem
 storiesOf("Button", module)
@@ -175,5 +176,21 @@ storiesOf("InterviewerList", module)
     <Error
       message="Could not delete appointment."
       onClose={action("onClose")}
+    />
+  ))
+  .add("Edit", () => (
+    <Form
+      student="Kevin"
+      interviewer={1}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ));
