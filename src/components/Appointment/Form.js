87e2433dd513349115a87e1da8acdf40a5d14752
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-06-23 13:28:17 -0700

    Add reset/cancel functions, implement cancel button

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,32 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import InterviewerList from 'components/InterviewerList';
 import Button from 'components/Button';
 
-export default function Form() {
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  
+  // clear the form values on cancel button click
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  // reset form and call onCancel
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form
+          autoComplete="off"
+          onSubmit={(event) => event.preventDefault()}
+        >
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-            your code goes here
-          */
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+            onCancel={props.onCancel}
+            onSave={props.onSave}
           />
         </form>
         <InterviewerList
-        /* your code goes here */
+          value={interviewer}
+          onChange={setInterviewer}
+          interviewers={props.interviewers}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger /* your code goes here */>Cancel</Button>
-          <Button confirm /* your code goes here */>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm /* */>Save</Button>
         </section>
       </section>
     </main>
