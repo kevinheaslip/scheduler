28c8095224f8e5a24f90b7142f9d15ce9ca9ff6f
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-06-23 21:51:33 -0700

    Add conditional rendering for Show and Empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 import 'components/Appointment/styles.scss';
 
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+
 export default function Appointment(props) {
   return (
-    <article className='appointment'></article>
+    <article className='appointment'>
+      {props.interview ?
+        <>
+          <Header time={props.time} />
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer.name}
+          />
+        </>
+        :
+        <>
+          <Header time={props.time} />
+          <Empty />
+        </>
+      }
+    </article>
   );
 }
