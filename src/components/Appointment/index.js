f85f995355132f13cc654a9fc1bd21fb34401f3b
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-07-01 15:05:37 -0700

    Add useVisualMode hook to manage mode, conditional rendering

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,24 +5,50 @@ import 'components/Appointment/styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
+
+import { useVisualMode } from 'hooks/useVisualMode';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
   return (
     <article className='appointment'>
-      {props.interview ?
+      {mode === EMPTY && (
+        <>
+          <Header time={props.time} />
+          <Empty onAdd={() => transition(CREATE)} />
+        </>
+      )}
+      {mode === SHOW && (
         <>
           <Header time={props.time} />
           <Show
             student={props.interview.student}
-            interviewer={props.interview.interviewer.name}
+            interviewer={props.interview.interviewer}
+          // onEdit={ }
+          // onDelete={ }
           />
         </>
-        :
+      )}
+      {mode === CREATE && (
         <>
           <Header time={props.time} />
-          <Empty />
+          <Form
+            student={props.student}
+            interviewer={props.interviewer}
+            interviewers={[]}
+            // onSave={ }
+            onCancel={() => back(EMPTY)}
+          />
         </>
-      }
+      )}
     </article>
   );
 }
